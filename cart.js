77b5ad1cdf2444a01499b1689aff38b90bd5c353
Riddhi Suteri <ridsuteri@gmail.com>
2026-06-21 12:44:05 +0530

    add eventlisteners

--- cart.js
+++ cart.js
@@ -1,11 +1,8 @@
 const cartContainer = document.querySelector(".cart-items-container");
-const cartTotalValue = document.querySelector('#cart-total-value');
+const cartTotalValue = document.querySelector("#cart-total-value");
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-
 loadCart();
-
-
 
 function loadCart() {
   if (cart.length > 0) {
@@ -27,6 +24,16 @@ function loadCart() {
       cartContainer.appendChild(cartItemDiv);
     });
 
+    const quantityInput = document.querySelectorAll(".quantity-input");
+    const removeBtn = document.querySelectorAll(".remove-button");
+
+    quantityInput.forEach((btn) => {
+      btn.addEventListener("change", handleQtyChange);
+    });
+    removeBtn.forEach((btn) => {
+      btn.addEventListener("click", handleRemove);
+    });
+
     const cartTotal = getCartTotal();
     cartTotalValue.innerHTML = cartTotal;
   } else {
@@ -36,8 +43,18 @@ function loadCart() {
 }
 
 function getCartTotal() {
-    const total = cart.reduce((acc, product)=>{
-        return acc += product.price * product.quantity;
-    },0)
-    return total;
+  const total = cart.reduce((acc, product) => {
+    return (acc += product.price * product.quantity);
+  }, 0);
+  return total;
+}
+
+function handleQtyChange() {
+    // change the qty in cart
+    // call the loadCart() function again
+}
+
+function handleRemove() {
+    // remove the product
+    // call the loadCart() function again
 }
